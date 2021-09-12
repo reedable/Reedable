4d925cbd100100b73d58bd96d78eee91ebf1fd26
Reedable <reedable@oinklet.com>
2021-09-12 14:36:51 -0400

    Commit all the assets created during the first submission to the web store.

--- build.sh
+++ build.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
+#
+# How to build and publish the extension
+#
+# Run this script, i.e.
+#
+#   $ ./build.sh
+#
+# Upload the resulting Reedable.zip file to
+#
+#   https://chrome.google.com/webstore/devconsole
+#
+# Justification for permissions
+#
+#   activeTab
+#     The extension applies the user's view preference to the currently active
+#     visited web site content.
+#
+#   storage
+#     The user's view preferences for the visited web site content are stored
+#     and shared across his browsers.
+#
+#   scripting
+#     The script modifies font and text spacing of the visited web
+#     site content.
+#
+#   tabs
+#     A the user switches to new tabs, the extension applies the user's view
+#     preference to the visited web site content.
+#
+# Justification for content scripts
+#
+#   The user may choose any sites for applying his view preferences.
+#
+
 
 zip -r Reedable.zip src
