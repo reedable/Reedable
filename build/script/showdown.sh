6c26f2d41e0a93b978d31249c33983e8cedb4db2
Reedable <reedable@oinklet.com>
2022-04-23 17:31:41 -0400

    Cleaning up the CSS.

---
+++ build/script/showdown.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+showdown='./node_modules/.bin/showdown'
+
+#${showdown} makehtml\
+#   -i docs/blog/2022/04/22/role_of_automated_unit_testing_over_sdlc.md \
+#   -o docs/blog/2022/04/22/role_of_automated_unit_testing_over_sdlc.html
